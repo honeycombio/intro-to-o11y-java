3c4c38117a73e62e078d0366b601a96a258ead89
Jessica Kerr <jessitron@gmail.com>
2021-08-18 15:17:21 -0500

    The Honeycomb SDK barfs all over with
    java.lang.IllegalAccessException: class io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent0 cannot access class jdk.internal.misc.Unsafe (in module java.base) because module java.base does not export jdk.internal.misc to unnamed module @548a102f
    at java.base/jdk.internal.reflect.Reflection.newIllegalAccessException(Reflection.java:361)
    at java.base/java.lang.reflect.AccessibleObject.checkAccess(AccessibleObject.java:591)
    at java.base/java.lang.reflect.Method.invoke(Method.java:558)
    at io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent0.run(PlatformDependent0.java:352)
    at java.base/java.security.AccessController.doPrivileged(Native Method)
    at io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent0.<clinit>(PlatformDependent0.java:343)
    at io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent.isAndroid(PlatformDependent.java:289)
    ....

--- src/main/java/io/honeycomb/otel/fibonacci/FibonacciApplication.java
+++ src/main/java/io/honeycomb/otel/fibonacci/FibonacciApplication.java
@@ -1,5 +1,7 @@
 package io.honeycomb.otel.fibonacci;
 
+import io.honeycomb.opentelemetry.HoneycombSdk;
+import io.honeycomb.opentelemetry.sdk.trace.samplers.DeterministicTraceSampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,7 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class FibonacciApplication {
 
+	
 	public static void main(String[] args) {
+
+		HoneycombSdk honeycomb = new HoneycombSdk.Builder()
+				.setApiKey(System.getenv("HONEYCOMB_API_KEY"))
+				.setDataset(System.getenv("HONEYCOMB_DATASET"))
+				.setSampler(new DeterministicTraceSampler(1))  // optional
+				.setServiceName("fibonacci-from-honeycomb-sdk")         // optional
+				.build();
+
 		SpringApplication.run(FibonacciApplication.class, args);
 	}
 
